1977d97b0abe114640e1a04f2db2f91ef398d895
JavDaGreat <98164907+JavDaGreat@users.noreply.github.com>
2023-07-22 14:27:30 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,7 +3,12 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyCQpgH1Txn9U6ZCL6652yF01HTyCrbRAwk",
+  authDomain: "chatt-app-95383.firebaseapp.com",
+  projectId: "chatt-app-95383",
+  storageBucket: "chatt-app-95383.appspot.com",
+  messagingSenderId: "725687925030",
+  appId: "1:725687925030:web:baea59cb3593e77ad769e7"
 };
 
 // Initialize Firebase
